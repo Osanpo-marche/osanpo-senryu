478c54585d6e72e8a5ccf75eb53636cd229ab7b5
Osanpo-marche <masanobu.imamura@gmail.com>
2026-01-06 17:08:23 +0900

    up

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+document.getElementById("postBtn").addEventListener("click", () => {
+  const name = document.getElementById("nickname").value;
+  const senryu = document.getElementById("senryu").value;
 
+  if (!senryu.trim()) {
+    alert("川柳を入力してね");
+    return;
+  }
+
+  alert("投稿（ダミー）\n" + (name || "散歩人") + "：\n" + senryu);
+});
